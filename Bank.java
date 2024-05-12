fdf1029f440afd23c21c04b9d9d341159a636236
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-12 22:53:51 +0900

    first commit

---
+++ Bank.java
@@ -0,0 +1,17 @@
+public class Bank {
+    public static void main(String[] args) {
+        int year;
+        int i;
+        double rate=1.05;
+        double money=Integer.parseInt(args[0]);
+        for (i=1;i<=10;i++){
+            rate=1.00+i*0.01;
+            money=10000.0;
+            for (year=0;year<100;year++){
+                money=money*rate;
+            } 
+        
+        System.out.println(i+"%の利子で"+year+"年後の預金は"+money+"円です");
+        }
+    }
+}
