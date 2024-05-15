24d8fb3b7198c473e1b65027acd7cb4b7efb6165
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-15 22:47:41 +0900

    declare Variable in java

---
+++ VarTest.java
@@ -0,0 +1,21 @@
+public class VarTest {
+    public static void main(String[] args){
+        int i; // (1)
+        i=1; // (2)
+        System.out.println(i + "の2乗は " + (i*i) + "です"); // (3)
+        i=i+1;
+        System.out.println(i+"の2乗は"+(i*i)+"です"); //(3)
+        i=i+1;
+        System.out.println(i+"の2乗は"+(i*i)+"です"); //(3)
+        i=i+1;
+        System.out.println(i+"の2乗は"+(i*i)+"です"); //(3)
+        i=i+1;
+        System.out.println(i+"の2乗は"+(i*i)+"です"); //(3)
+        i=i+1;
+        System.out.println(i+"の2乗は"+(i*i)+"です"); //(3)
+        i=i+1;
+        System.out.println(i+"の2乗は"+(i*i)+"です"); //(3)
+        i=i+1;
+        System.out.println(i+"の2乗は"+(i*i)+"です"); //(3)
+    }
+}
---
+++ VarTest2.java
@@ -0,0 +1,7 @@
+public class VarTest2 {
+    public static void main(String[] args) {
+        int i;
+        for (i=1;i<10;i++)
+          System.out.println(i+" の2乗は "+(i*i)+"です");
+    }
+}
