68f3f0cd8b32436463c8c0ecbb4d653e51e00980
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-18 01:46:02 +0900

    how to concat string

---
+++ ConcatSample.java
@@ -0,0 +1,9 @@
+public class ConcatSample {
+  static public void main(String args[]){
+    String str1 = "文字列1";
+    String str2 = "文字列2";
+    String str = str1 + str2;
+    System.out.println("+演算子による文字列の連結 -> " + str);
+  }
+}
+
