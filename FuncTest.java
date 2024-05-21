105bab9403b91f7d19240da6a6cefe44176bb54d
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-22 00:27:40 +0900

    how to use Math function

---
+++ FuncTest.java
@@ -0,0 +1,10 @@
+public class FuncTest {
+    public static void main(String[] args) {
+        System.out.print("sin(1)は");
+        System.out.print(Math.sin(1));
+        System.out.println("です");
+        System.out.print("2の平方根は");
+        System.out.print(Math.sqrt(2));
+        System.out.println("です");
+    }
+}
