7c93a82ea570acd0fad2a29c4b7b1344a59fbfe9
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-22 22:35:14 +0900

    second to hours

---
+++ Hours.java
@@ -0,0 +1,7 @@
+public class Hours {
+    public static void main(String[] args) {
+        System.out.print("10000秒は");
+        System.out.print(10000.0/(60*60));
+        System.out.println("時間です. ");
+    }
+}
