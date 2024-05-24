16664db9fd4f902cabb45967b91d350dea35d2a2
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-25 01:27:07 +0900

    special name

---
+++ MyName2.java
@@ -0,0 +1,17 @@
+public class MyName2 {
+    public static void main(String[] args) {
+        System.out.println("TTTTTTT    TTTTTTT");
+        System.out.println("   T          T");
+        System.out.println("   T          T");
+        System.out.println("   T          T");
+        System.out.println("   T          T");
+        System.out.println("   T          T");
+
+        int i;
+        System.out.println("TTTTTTT    TTTTTTT");
+        for (i=0; i<3; i++) //for (i=1;i<=回数;i++)と書いても同じだがi=0の方が好まれるらしい
+          System.out.println("   T          T");
+        for(i=0;i<2;i++)
+          System.out.println("   T    ..    T");
+    }
+}
