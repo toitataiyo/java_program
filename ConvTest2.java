39630f2b84aa63105d25df94ee2d918d031624ed
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-26 20:31:51 +0900

    how to Rounding off

---
+++ ConvTest2.java
@@ -0,0 +1,10 @@
+public class ConvTest2 {
+    public static void main(String[] args) {
+        long i = 0, j;
+        double f = -1.9, g;
+        j = (int)Math.rint(f);
+        g=i;
+        g=(double)i;
+        System.out.println("j="+j+",g="+g);
+    }
+}
