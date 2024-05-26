56d8d3cd3456efc4edc8a01d440c032c953fcc1b
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-26 20:34:50 +0900

    add comment about init

--- SumArgs.java
+++ SumArgs.java
@@ -1,6 +1,6 @@
 public class SumArgs {
     public static void main(String[] args){
-        int sum=0, num;
+        int sum=0, num; //初期化(sum)と宣言(num)を同時に行なっている//
         int i;
         for(i=0; i< args.length; i++){
             num=Integer.parseInt(args[i]);
