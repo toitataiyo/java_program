9e97470ff0e97d26097c59979b6d5bcdb5bdaa70
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-26 20:04:34 +0900

    about type casting

---
+++ ConvTest.java
@@ -0,0 +1,10 @@
+public class ConvTest {
+    public static void main(String[] args) {
+        long i=0, j;
+        double f = -1.9, g;
+        j=(int)f;
+        g=i;
+        g=(double)i;
+        System.out.println("j="+j+",g="+g);
+    }
+}
