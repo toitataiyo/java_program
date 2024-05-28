5dc74538e2f7b6c555fabd29dcb320a9d0e25ac3
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-28 23:30:56 +0900

    how to express big string

---
+++ MyName.java
@@ -0,0 +1,11 @@
+class MyName {
+    public static void main(String[] args) {
+        System.out.println("_____ _____/  _____ _____/");
+        System.out.println("     /             /");
+        System.out.println("    /             /");
+        System.out.println("   /             / ");
+        System.out.println("  /   /         /  ");
+        System.out.println("_/  _/        _/");
+        ;
+    }
+}
