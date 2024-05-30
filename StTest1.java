40cd94a357bc9f1e11b1ae059c314620afd8c4c6
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-05-30 23:35:05 +0900

    equals method

---
+++ StTest1.java
@@ -0,0 +1,10 @@
+public class StTest1 {
+    public static void main(String[] args) {
+        if(args[0].equals("tanaka")){
+            System.out.println("あなたはおそらく田中さんですね(棚香かも). ");
+        }
+        else{
+            System.out.println("あなたは田中ではない");
+        }
+    }
+}
