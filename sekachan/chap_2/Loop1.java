c15b9177423c33078a9e9b72704f7abc8377e30f
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-06-25 15:15:55 +0900

    how to use while loop

---
+++ sekachan/chap_2/Loop1.java
@@ -0,0 +1,24 @@
+
+public class Loop1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<3) {
+            System.out.println("i= "+i);
+            i++;
+        }
+    }
+}
+
+
+/*
+while文
+while (条件式){
+    処理
+}
+
+for文
+for(式1;条件式;式2){
+    処理
+}
+
+ */
