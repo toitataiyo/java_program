f2c671145206d7bd7867d047e2eeb49d79189a41
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-07-05 19:12:04 +0900

    change bank rate

--- Bank.java
+++ Bank.java
@@ -5,7 +5,7 @@ public class Bank {
         double rate=1.05;
         double money=Integer.parseInt(args[0]);
         for (i=1;i<=10;i++){
-            rate=1.00+i*0.01;
+            rate=1.00+i*0.1;
             money=10000.0;
             for (year=0;year<100;year++){
                 money=money*rate;
