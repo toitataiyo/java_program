9713f19079e5d98066696dd4723e96d3c665b41b
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-07-05 19:48:04 +0900

    how to concat1

---
+++ sample_program/ConcatSample1.java
@@ -0,0 +1,10 @@
+package sample_program;
+
+public class ConcatSample1 {
+    public static void main(String[] args) {
+        String str1 = "文字列1";
+        String str2 = "文字列2";
+        String str = str1+str2;
+        System.out.println("+演算子による文字列の連結 ->" +str);
+    }
+}
