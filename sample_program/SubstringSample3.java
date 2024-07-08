a35ab2e568ab5bf82793da1ed33414e9aa004966
toitataiyo <s202065d@yokohama-cu.ac.jp>
2024-07-09 01:20:28 +0900

    fix mistake

--- sample_program/SubstringSample3.java
+++ sample_program/SubstringSample3.java
@@ -3,7 +3,7 @@ public class SubstringSample3 {
         String str = "壱２34伍６７89０";
         System.out.println("文字列の6文字目から最後までを取り出す -> "
                            +str.substring(6-1));
-        System.out.println("文字列の後ろ3も自分を取り出す -> "
+        System.out.println("文字列の後ろ3文字分を取り出す -> "
                             + str.substring(str.length()-3));
     }
 }
